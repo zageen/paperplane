0654c021be68747db7352095a8713e6f2ef2be82
zageen <zageen60@gmail.com>
2019-01-13 15:20:56 +0100

    musique Changes

---
+++ Assets/Scripts/MusiqueManagment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class MusiqueManagment : MonoBehaviour {
+
+    public AudioSource audioSource1;
+    public float audioSource1Duration =17.22f;
+    public AudioSource audioSource2;
+
+    private void Start()
+    {
+        StartCoroutine(MusiqueManager());
+    }
+
+    IEnumerator MusiqueManager()
+    {
+        audioSource1.Play();
+        yield return new WaitForSeconds(audioSource1Duration);
+        audioSource2.Play();
+    }
+}
