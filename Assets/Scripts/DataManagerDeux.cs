cdda84c76a5f6704177f568f8af6d5f2afcd131c
zageen <zageen60@gmail.com>
2019-01-14 18:15:34 +0100

    end level managment + sceneMenu merge

---
+++ Assets/Scripts/DataManagerDeux.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+
+public class DataManagerDeux : MonoBehaviour {
+
+    public static DataManagerDeux Instance;
+
+    public string path;
+
+    public Data data;
+	
+	void Awake ()
+    {
+
+        Instance = this;
+        SetPath();
+        Load();
+	}
+
+    void SetPath()
+    {
+        path = Path.Combine(Application.persistentDataPath, "data.json");
+    }
+
+    public void Save ()
+    {
+        SetData();
+        SerializeData();
+
+	}
+
+
+    void SetData()
+    {
+        if( data == null)
+        {
+            data = new Data();
+        }
+
+        data.index = GameManagerDeux.Instance.index;
+    }
+
+
+    void SerializeData()
+    {
+        string dataString = JsonUtility.ToJson(data, true);
+        File.WriteAllText(path, dataString);
+    }
+
+    public void Load()
+    {
+        if (File.Exists(path))
+        {
+            DeserializeData();
+        }
+        else
+        {
+            data = new Data();
+        }
+
+        ExploitData();
+
+    }
+
+
+    void DeserializeData()
+    {
+        string loadedString = File.ReadAllText(path);
+        data = JsonUtility.FromJson<Data>(loadedString);
+    }
+
+    void ExploitData()
+    {
+
+        GameManagerDeux.Instance.index = data.index;
+
+    }
+
+    
+}
---
+++ Assets/Scripts/GameManagerDeux.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerDeux : MonoBehaviour {
+
+    
+    public Renderer cubeRenderer;
+    public Material[] materials;
+    public int index;
+
+    public static GameManagerDeux Instance;
+
+    void Awake()
+    {
+
+        Instance = this;
+
+    }
+
+    void Start()
+    {
+        DataManagerDeux.Instance.Load();
+        cubeRenderer.material = materials[index];
+    }
+
+    void Update()
+    {
+        MaterialChangeUpdate();
+    }
+
+    private void MaterialChangeUpdate()
+    {
+        // change la méthode pour avoir le score
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            cubeRenderer.material = materials[index];
+
+            DataManagerDeux.Instance.Save();
+
+            index++;
+
+
+            if (index > materials.Length - 1)
+            {
+                index = 0;
+            }
+
+
+        }
+    }
+
+    public void OnApplicationPause(bool pause)
+    {
+        DataManagerDeux.Instance.Save();
+    }
+}
